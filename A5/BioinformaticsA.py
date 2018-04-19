2fbfe3ae83965582d702388e30bd3426b6f26274
Emily Sawtelle <es2715@mcla.edu>
2018-04-19 23:36:38 +0000

    adding A5

---
+++ A5/BioinformaticsA.py
@@ -0,0 +1,38 @@
+#Program A5, by Emily Sawtelle
+#coded for bioinformatics 
+
+gencode = {
+ 'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
+ 'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
+ 'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
+ 'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
+ 'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
+ 'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
+ 'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
+ 'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
+ 'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
+ 'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
+ 'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
+ 'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
+ 'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
+ 'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
+ 'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_',
+ 'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W'}
+ 
+#above was prompted in assignment
+
+def translate_dna(sequence):
+    codons=[]
+    #codons hold codons
+    amnino=[]
+    #amino holds amino acids from the dictionary
+    codons=[sequence[i:i+3] for i in range(0, len(sequence), 3)]
+    #splits the string into codons to be read properly
+    
+    for c in codons:
+        amnino.append(gencode.get(c))
+    return amnino
+    
+    #c in loop gets amino acids from the dictionary
+    
+    
---
+++ A5/Test.py
@@ -0,0 +1,11 @@
+#Emily Sawtelle, Bioinformatics
+# Program A5
+import BioinformaticsA
+
+# input sequence is easy
+
+print BioinformaticsA.translate_dna("ATGTTCGGT") 
+print BioinformaticsA.translate_dna("ACAACCAGT") 
+print BioinformaticsA.translate_dna("GACTCCTTTGAGGGA") 
+
+#this was given to us
